1e1c6c5d8397abf83eb1356134f9ebf2eb5c765c
Your Name <fw00050@surrey.ac.uk>
2012-01-11 11:36:18 +0000

    little fix

--- config/routes.rb
+++ config/routes.rb
@@ -11,15 +11,6 @@ RailsApp::Application.routes.draw do
 
   root :to => 'pages#home'
 
-  get "pages/home"
-
-  get "pages/enquiry"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/survey"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
